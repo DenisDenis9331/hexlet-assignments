c07539fdfcb5bad6f03d8f9134817786dce7a6c9
@hexlet/cli <support@hexlet.io>
2023-03-06 23:07:24 +0500

    submit rails-basics-ru/templates

---
+++ rails-basics-ru/templates/app/models/task.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Task < ApplicationRecord
+  validates :name, :status, :creator, :completed, presence: true
+
+  after_initialize :set_default, if: :new_record?
+
+  private
+
+  def set_default
+    self.status ||= 'new'
+  end
+end
