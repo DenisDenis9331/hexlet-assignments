b15f3b343c91893241aa0dc462bb1903dbab6508
@hexlet/cli <support@hexlet.io>
2023-03-09 23:50:05 +0500

    submit rails-basics-ru/templates

--- rails-basics-ru/templates/app/models/task.rb
+++ rails-basics-ru/templates/app/models/task.rb
@@ -1,8 +1,10 @@
 # frozen_string_literal: true
 
 class Task < ApplicationRecord
-  validates :name, :status, :creator, :completed, presence: true
+  validates :name, :status, presence: true
 
+  validates_length_of :creator, maximum: 32, message: "less than 32 if you don't mind"
+  validates_length_of :performer, maximum: 32, message: "less than 32 if you don't mind"
   validates :completed, inclusion: { in: [true, false] }
 
   after_initialize :set_default, if: :new_record?
